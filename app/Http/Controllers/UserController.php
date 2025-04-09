77e5379043430314220a551e046a1edd3bf6f3d7
Lydia Amador Cuenca <lydiaamadorcuenca@gmail.com>
2025-04-09 16:50:10 +0200

    Version 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -42,6 +43,10 @@ class UserController extends Controller
             'usudiareparto'=>'nullable|string|max:20',
             'usunif'=>'nullable|string|max:20'
         ]);
+
+        $user = User::create($data);
+
+        return response()->json($user,201);
     }
 
     /**
@@ -50,6 +55,7 @@ class UserController extends Controller
     public function show(string $id)
     {
         //
+        return User::findOrFail($id);
     }
 
     /**
@@ -58,6 +64,31 @@ class UserController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $user = User::findOrFail($id);
+
+        $data= $request = validate([
+            'name' => 'required|string|max:255',
+            'email'=> 'required|string|max:255',
+            'email_verified_at' =>'nullable|timestamp',
+            'password' => 'required|string|max:255',
+            'usugrucod'=> 'requiered|string|max:255',
+            'usuclicod'=>'requiered|string|max:255',
+            'usucencod'=>'nullable|string|max:4',
+            'remember_token'=>'nullable|string|max:100',
+            'usutarcod'=>'requiered|string|max:3',
+            'usuofecod'=>'nullable|string|max:10',
+            'usudocpen'=>'requiered|float',
+            'usudes1'=>'nullable|float',
+            'usunuevo'=>'nullable|integer|max:11',
+            'usurprcod'=>'nullable|string|max:15',
+            'usuivacod'=>'nullable|string|max:3',
+            'usudistribuidor'=>'nullable|int|max:11',
+            'usudiareparto'=>'nullable|string|max:20',
+            'usunif'=>'nullable|string|max:20'
+        ]); 
+
+        $user->update($data);
+        return response->json($user);
     }
 
     /**
@@ -66,5 +97,9 @@ class UserController extends Controller
     public function destroy(string $id)
     {
         //
+        $user = User::findOrFail($id);
+        $user = delete();
+
+        return response->json(['mensaje' => 'Cliente eliminado correctamente.']);
     }
 }
