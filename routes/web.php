7e2e672f7df76bf996b911320c23f7da97ca2818
jesus lavado <kjblpo@gmail.com>
2025-04-09 13:56:11 +0200

    Modelo y Controlador de Pedido

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PedidosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/pedidos', [PedidosController::class, 'index']);
+Route::get('/pedidos/cliente/{cliente_id}', [PedidosController::class, 'pedidosPorCliente']);
+
+
+
