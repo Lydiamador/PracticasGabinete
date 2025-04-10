65e1fec2c6f6ff2655a785134b84ef3dc95befe7
jesus lavado <kjblpo@gmail.com>
2025-04-10 09:23:44 +0200

    Modificaciones en los Controladores

--- app/Http/Controllers/Pedidos_LineasController.php
+++ app/Http/Controllers/Pedidos_LineasController.php
@@ -19,12 +19,66 @@ class Pedidos_LineasController extends Controller
 
     // Crear una nueva linea de pedido
     public function store(Request $request){
-        return Pedidos_Lineas::create($request->all());
+        //validamos los datos a introducir del pedido con los campos de la tabla
+        $data= $request = validate([
+            'id'=>'required|integer|auto_increment|max:11',
+            'pedido_id'=>'required|integer|max:11',
+            'id_listaregalos'=>'required|integer|max:11',
+            'producto_id'=>'nullable|integer|max:11',
+            'variante_id'=>'nullable|integer|max:11',
+            'producto_ref'=>'required|string|max:50',
+            'cantidad'=>'nullable|decimal|max:9,2',
+            'precio'=>'nullable|decimal|max:6,2',
+            'impuesto'=>'required|string|max:3',
+            'impuesto_tasa'=>'required|decimal|max:5,2',
+            'impuesto_re'=>'required|decimal|max:5,2',
+            'observaciones'=>'nullable|string|max:1000',
+            'aclcancaj'=>'nullable|float',
+            'aclcanjcod'=>'nullable|string|max:4|min:4',
+            'nombre_articulos'=>'nullable|string|max:100',
+            'iva'=>'required|double',
+            'iva_porcentaje'=>'required|double',
+            'recargo'=>'required|double',
+            'recargo_porcentaje'=>'required|double',
+            'es_app'=>'nullable|integer|max:11',
+        ]);
+        //creamos el pedido
+        $pedido = Pedidos_Lineas::create($data);
+       // Devolvemos el pedido creado
+       return response()->json($pedido,201);
     }
 
     // Actualizar una linea del pedido
     public function update(Request $request){
-        return Pedidos_Lineas::find($request->$id)->update($request->all());
+
+        $pedido = Pedidos_Lineas::find($request->id);
+
+        //validamos los datos a introducir del pedido con los campos de la tabla
+        $data= $request = validate([
+            'id'=>'required|integer|auto_increment|max:11',
+            'pedido_id'=>'required|integer|max:11',
+            'id_listaregalos'=>'required|integer|max:11',
+            'producto_id'=>'nullable|integer|max:11',
+            'variante_id'=>'nullable|integer|max:11',
+            'producto_ref'=>'required|string|max:50',
+            'cantidad'=>'nullable|decimal|max:9,2',
+            'precio'=>'nullable|decimal|max:6,2',
+            'impuesto'=>'required|string|max:3',
+            'impuesto_tasa'=>'required|decimal|max:5,2',
+            'impuesto_re'=>'required|decimal|max:5,2',
+            'observaciones'=>'nullable|string|max:1000',
+            'aclcancaj'=>'nullable|float',
+            'aclcanjcod'=>'nullable|string|max:4|min:4',
+            'nombre_articulos'=>'nullable|string|max:100',
+            'iva'=>'required|double',
+            'iva_porcentaje'=>'required|double',
+            'recargo'=>'required|double',
+            'recargo_porcentaje'=>'required|double',
+            'es_app'=>'nullable|integer|max:11',
+        ]);
+        //actualizamos el pedido
+        $pedido->update($data);
+        return response->json($pedido);
     }
 
     // Eliminar una linea del pedido
